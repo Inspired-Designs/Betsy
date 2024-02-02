308333de78bba62ca4444480a9f88514a3b16cc7
Rogelio <roger.cruz808@gmail.com>
2024-02-02 12:39:33 -0800

    Dynamic styling added to the navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,45 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const Navbar = () => {
+    
+  const [isHovered, setIsHovered] = useState(false)
+  const [isSignInHovered, setIsSignInHovered] = useState(false)
+  const [isCartHovered, setIsCartHovered] = useState(false)
+  
+  
+  const HandleCategoryEnter = () => {
+    setIsHovered(true)
+  }
+  
+  const HandleCategoryLeave = () => {
+    setIsHovered(false)
+  }
+
+  const HandleSignInEnter = () => {
+    setIsSignInHovered(true)
+  }
+  
+  const HandleSignInLeave = () => {
+    setIsSignInHovered(false)
+  }
+
+  const HandleCartEnter = () => {
+    setIsCartHovered(true)
+  }
+  
+  const HandleCartLeave = () => {
+    setIsCartHovered(false)
+  }
+
+
   return (
     <div className="Navbar">
       <img className="Logo" src="./images/Navbar/logo.png" />
-      <div className="CategoryContainer">
+      <div 
+        className={`CategoryContainer ${isHovered ? 'hovered' : ''}`}
+        onMouseEnter={HandleCategoryEnter}
+        onMouseLeave={HandleCategoryLeave}
+      >
         <img className="Burger" src="./images/Navbar/burger.png" />
         <p>Categories</p>
       </div>
@@ -12,10 +47,19 @@ const Navbar = () => {
         <input className="SearchInput" type="text" placeholder="Search for anything" />
         <img className="SearchButton" src="./images/Navbar/search-button.png" />
       </div>
-      <div className="SignInButton">
+      <div 
+        className={`SignInButton ${isSignInHovered ? 'hovered' : ''}`}
+        onMouseEnter={HandleSignInEnter}
+        onMouseLeave={HandleSignInLeave}
+      >
         <p className="SignIn">Sign In</p>
       </div>
-      <div className="CartDiv">
+      <div 
+        // className="CartDiv"
+        className={`CartDiv ${isCartHovered ? 'hovered' : ''}`}
+        onMouseEnter={HandleCartEnter}
+        onMouseLeave={HandleCartLeave}
+      >
       <img className="CartButton" src="./images/Navbar/cart.png" />
       </div>
     </div>
