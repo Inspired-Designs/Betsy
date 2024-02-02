2e9ce99897c129a3c29a80fedb4449ca7763087c
Rogelio <roger.cruz808@gmail.com>
2024-02-02 15:51:54 -0800

    navbar complete

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import Navbar from "./components/Navbar";
-import Styles from "./Navbar.css"
+import Styles from "./styles/Navbar.css"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
