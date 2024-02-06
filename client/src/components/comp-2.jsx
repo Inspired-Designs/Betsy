f8f360ab0c3af4ca1aee1067b0b64c7e74c5fd1b
Rogelio <roger.cruz808@gmail.com>
2024-02-06 08:31:54 -0800

    started comp 2

---
+++ client/src/components/comp-2.jsx
@@ -0,0 +1,18 @@
+import React, { Fragment, useState } from "react";
+
+const ComponentTwo = () => {
+  return (
+    <Fragment>
+      <p>In 20+ carts</p>
+      {/* price */}
+      {/* description */}
+      {/* bs deliveries */}
+      {/* select an option */}
+      {/* select an option */}
+      {/* Add to cart button */}
+      {/* star seller */}
+    </Fragment>
+  )
+}
+
+export default ComponentTwo
