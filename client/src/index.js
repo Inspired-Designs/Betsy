816351b7065aab9e67ede15053cfd04d19912089
Rogelio <roger.cruz808@gmail.com>
2024-02-06 08:31:54 -0800

    started comp 2

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import Navbar from "./components/Navbar";
 import Styles from "./styles/Navbar.css"
+import ComponentTwo from "./components/comp-2";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Navbar />
+    <ComponentTwo />
     <App />
   </React.StrictMode>
 );
