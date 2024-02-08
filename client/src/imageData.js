a29da48b1a58c521c703025225915089b7ed99e0
adrian <59178218+Doctor-san@users.noreply.github.com>
2024-02-08 13:54:13 -0700

    added Image-Gallery components

---
+++ client/src/imageData.js
@@ -0,0 +1,9 @@
+const imageLinks = [
+    'https://img.freepik.com/premium-photo/dog-wearing-gold-chain-gold-chain_769074-849.jpg?w=996',
+    'https://i5.walmartimages.com/asr/ad5faebf-9401-49d8-8d56-44cc41de99b8.1e857edd7f42182776e1c487c903701f.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF',
+    'https://cdn.shopify.com/s/files/1/0603/9181/7468/files/What_are_Necklace_for_Dogs.jpg?v=1665225417',
+    'https://www.frenchiely.com/cdn/shop/products/creative_comfy_hoodies_sweater_coat_for_medium_dogs_french_bulldogs_346.jpg?v=1684989243&width=990',
+    'https://external-preview.redd.it/vrk9hRoI1ACdG0jLRtEQzGoP3CiOjr4U6-WnpL5cnPM.jpg?auto=webp&s=ccc9edc1ecc4ee70cebd991fb2de6c8b84a60662'
+];
+
+export default imageLinks;
