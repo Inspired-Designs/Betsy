35d803ffe5b3ad3bfce68b4db8ffa4239f11bcae
Rogelio <roger.cruz808@gmail.com>
2024-02-14 14:59:42 -0800

    price rendered

--- server/server.js
+++ server/server.js
@@ -28,78 +28,78 @@ app.get("/items", async (req, res) => {
   }
 });
 
-app.get("/reviews", async (req, res) => {
-  try {
-    let data = await db.query("SELECT * FROM reviews;");
-    res.status(200).json(data.rows);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("could not retrieve review information");
-  }
-});
+// app.get("/reviews", async (req, res) => {
+//   try {
+//     let data = await db.query("SELECT * FROM reviews;");
+//     res.status(200).json(data.rows);
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).send("could not retrieve review information");
+//   }
+// });
 
-app.get("/reviews/:id", async (req, res) => {
-  try {
-    let id = req.params;
-    let data = await db.query("SELECT * FROM reviews WHERE review_id = $1", [id]);
-    if (data.rows.length === 0) {
-      res.status(404).send("Review not found");
-    } else {
-      res.status(200).json(data.rows[0]);
-    }
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("could not retrieve review information");
-  }
-});
+// app.get("/reviews/:id", async (req, res) => {
+//   try {
+//     let id = req.params;
+//     let data = await db.query("SELECT * FROM reviews WHERE review_id = $1", [id]);
+//     if (data.rows.length === 0) {
+//       res.status(404).send("Review not found");
+//     } else {
+//       res.status(200).json(data.rows[0]);
+//     }
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).send("could not retrieve review information");
+//   }
+// });
 
-app.get("/sellers", async (req, res) => {
-  try {
-    let data = await db.query("SELECT * FROM sellers");
-    res.status(200).json(data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("could not retrieve seller information");
-  }
-});
+// app.get("/sellers", async (req, res) => {
+//   try {
+//     let data = await db.query("SELECT * FROM sellers");
+//     res.status(200).json(data);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("could not retrieve seller information");
+//   }
+// });
 
-app.get("/shipping", async (req, res) => {
-  try {
-    let data = await db.query("SELECT * FROM shipping");
-    res.status(200).json(data.rows[0]);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("could not retrieve shipping information");
-  }
-});
+// app.get("/shipping", async (req, res) => {
+//   try {
+//     let data = await db.query("SELECT * FROM shipping");
+//     res.status(200).json(data.rows[0]);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("could not retrieve shipping information");
+//   }
+// });
 
-app.get("/pictures", async (req, res) => {
-  try {
-    let data = await db.query("SELECT * FROM pictures");
-    console.log(data, "pictures data");
-    res.status(200).json(data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("could not retrieve images information");
-  }
-});
+// app.get("/pictures", async (req, res) => {
+//   try {
+//     let data = await db.query("SELECT * FROM pictures");
+//     console.log(data, "pictures data");
+//     res.status(200).json(data);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("could not retrieve images information");
+//   }
+// });
 
-app.get("/pictures/:id", async (req, res) => {
-  try {
-    console.log(req.params.id, "req params pictures");
-    let id = req.params.id;
-    let data = await db.query("SELECT FROM pictures WHERE picture_id = $1", [id]);
-    console.log(data, "picture data");
-    if (data.rows.length === 0) {
-      res.status(404).send("Image not found");
-    } else {
-      res.status(200).json(data.rows[0]);
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal server error");
-  }
-});
+// app.get("/pictures/:id", async (req, res) => {
+//   try {
+//     console.log(req.params.id, "req params pictures");
+//     let id = req.params.id;
+//     let data = await db.query("SELECT FROM pictures WHERE picture_id = $1", [id]);
+//     console.log(data, "picture data");
+//     if (data.rows.length === 0) {
+//       res.status(404).send("Image not found");
+//     } else {
+//       res.status(200).json(data.rows[0]);
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("Internal server error");
+//   }
+// });
 
 function logger(req, res, next) {
   console.log(`The request method was ${req.method}`);
