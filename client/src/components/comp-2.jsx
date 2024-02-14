c595ab6307cc47ba505dc3809dcae6b835731e5c
Rogelio <roger.cruz808@gmail.com>
2024-02-14 15:08:09 -0800

    description renders

--- client/src/components/comp-2.jsx
+++ client/src/components/comp-2.jsx
@@ -1,8 +1,30 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 
 const ComponentTwo = () => {
 
-  let price = "$129.99"
+  const [priceData, setPriceData] = useState('');
+  const [descriptionData, setDescriptionData] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/items');
+        if (!response.ok) {
+          throw new Error('Failed to fetch shipping data');
+        }
+        const data = await response.json();
+        console.log('data', data[0].price)
+        setPriceData(data[0].price);
+        setDescriptionData(data[0].item_description)
+      } catch (error) {
+        console.error('Error fetching shipping data', error)
+      }
+    }
+    
+    fetchData();
+  }, []);
+
+
   let description = "Introducing our versatile chain collection, available in gold, silver, and stainless steel finishes, with lengths ranging from 14mm to 18mm, ensuring there's a perfect match for every style and occasion. Crafted with precision and durability in mind, our chains offer timeless elegance and effortless customization to elevate your look with ease."
   let finish = ['Gold', 'Silver', 'Stainless Steel']
   let length = ['12mm', '16mm', '18mm']
@@ -10,9 +32,9 @@ const ComponentTwo = () => {
   return (
     <div className="Info">
       <p style={{ color: 'darkred' }}>In 20+ carts</p>
-      <h3 style={{ color: 'green' }}>{price}</h3>
+      <h3 style={{ color: 'green' }}>{priceData}</h3>
       <h6 style={{ color: 'green' }}>20% off sale for the next 4 hours</h6>
-      <p className="Description">{description}</p>
+      <p className="Description">{descriptionData}</p>
       <ul>
         <li>Arrives soon! Get it in less than 24 hours</li>
         <li>Returns & exchanges accepted</li>
